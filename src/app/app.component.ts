0752dbf66c4dd81c6b8f460ac80d2f6187e95b8f
wapec <bohdan.wapec@gmail.com>
2017-08-04 20:04:15 +0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  config: Object = {
+    pagination: '.swiper-pagination',
+    effect: 'coverflow',
+    autoplay: 3000,
+    slidesPerView: 1,
+    paginationClickable: true,
+    spaceBetween: 0,
+    // mousewheelControl: true,
+    autoplayDisableOnInteraction: true,
+    loop: true
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+
+import { SwiperModule } from 'angular2-useful-swiper';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SwiperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
