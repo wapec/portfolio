d997d95c88bdc48b44ecca7d885bbcff37e4a5b5
wapec <bohdan.wapec@gmail.com>
2017-08-08 00:58:55 +0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 
-  config: Object = {
-    pagination: '.swiper-pagination',
-    effect: 'coverflow',
-    autoplay: 3000,
-    slidesPerView: 1,
-    paginationClickable: true,
-    spaceBetween: 0,
-    // mousewheelControl: true,
-    autoplayDisableOnInteraction: true,
-    loop: true
-  };
+  // config: Object = {
+  //   pagination: '.swiper-pagination',
+  //   effect: 'coverflow',
+  //   autoplay: 3000,
+  //   slidesPerView: 1,
+  //   paginationClickable: true,
+  //   spaceBetween: 0,
+  //   // mousewheelControl: true,
+  //   autoplayDisableOnInteraction: true,
+  //   loop: true
+  // };
 }
